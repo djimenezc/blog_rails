a1441557fd43984e3df6e764055e42b685e69ce5
David Jimenez <david.calvo@clavistechnology.com>
2014-11-07 15:37:14 +0000

    Initial commit
    - blog project created using Rails scaffolding tool

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '1f836671c3320cb0fb93db9e51a34dd47cf3bf560280825b63842b8a8c25395a0511d7716fceccd9ac16964913817f1d123ea3fb7f6ffa0c7ac52a780be65eb3'
