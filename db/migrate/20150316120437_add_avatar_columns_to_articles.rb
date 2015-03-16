ca8f4e173f33d9db7792143ad47d08a55b8c9af7
David Jimenez <david.calvo@clavistechnology.com>
2015-03-16 14:47:22 +0000

    - Added migration file to create attachment field in the article entity

---
+++ db/migrate/20150316120437_add_avatar_columns_to_articles.rb
@@ -0,0 +1,9 @@
+class AddAvatarColumnsToArticles < ActiveRecord::Migration
+  def self.up
+    add_attachment :articles, :avatar
+  end
+
+  def self.down
+    remove_attachment :articles, :avatar
+  end
+end
