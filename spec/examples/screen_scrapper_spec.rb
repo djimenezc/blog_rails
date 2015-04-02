a1ded2ebb0ea4c8c5a6f00edb23793afdda2829e
David Jimenez <david.calvo@clavistechnology.com>
2015-04-02 12:43:28 +0100

    - Screen Scrapper: reading image example

--- spec/examples/screen_scrapper_spec.rb
+++ spec/examples/screen_scrapper_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
 describe ScreenScrapperService do
 
   it 'Read walmart title after a search' do
-
-    require 'rubygems'
-    require 'nokogiri'
-    require 'open-uri'
 
     url = 'http://www.walmart.com/search/search-ng.do?search_constraint=0&ic=48_0&search_query=Batman&Find.x=0&Find.y=0&Find=Find'
     doc = Nokogiri::HTML(open(url))
@@ -15,4 +15,17 @@ describe ScreenScrapperService do
 
     doc.at_css('title').text.should eq 'Batman - Walmart.com'
   end
+
+  # it 'Get an example image' do
+  #
+  #   PAGE = 'http://www.marketwatch.com'
+  #
+  #   html = Nokogiri.HTML(open(PAGE))
+  #
+  #   src  = html.at('#logo')['background-image']['url']
+  #
+  #   File.open('foo.png', 'wb') do |f|
+  #     f.write(open(src).read)
+  #   end
+  # end
 end
