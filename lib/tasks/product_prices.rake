bf14959a855e7a37b1926db72c39cc284316b530
David Jimenez <david.calvo@clavistechnology.com>
2015-04-03 15:30:38 +0100

    - Add a rake task to update the product price

---
+++ lib/tasks/product_prices.rake
@@ -0,0 +1,17 @@
+desc 'Fetch product prices'
+
+# http://asciicasts.com/episodes/190-screen-scraping-with-nokogiri
+
+task :fetch_prices => :environment do
+
+  require 'nokogiri'
+  require 'open-uri'
+
+  Product.find_all_by_price(nil).each do |product|
+    escaped_product_name = CGI.escape(product.name)
+    url = "http://www.walmart.com/search/search-ng.do?search_constraint=0&ic=48_0&search_query=#{escaped_product_name}&Find.x=0&Find.y=0&Find=Find"
+    doc = Nokogiri::HTML(open(url))
+    price = doc.at_css('.PriceXLBold, .PriceCompare .BodyS').text[/[0-9\.]+/]
+    product.update_attribute(:price, price)
+  end
+end
--- spec/examples/screen_scrapper_spec.rb
+++ spec/examples/screen_scrapper_spec.rb
@@ -6,26 +6,38 @@ require 'open-uri'
 
 describe ScreenScrapperService do
 
-  it 'Read walmart title after a search' do
+  # it 'Read walmart title after a search' do
+  #
+  #   url = 'http://www.walmart.com/search/search-ng.do?search_constraint=0&ic=48_0&search_query=Batman&Find.x=0&Find.y=0&Find=Find'
+  #   doc = Nokogiri::HTML(open(url))
+  #
+  #   puts "Title: #{doc.at_css('title').text}"
+  #
+  #   doc.at_css('title').text.should eq 'Batman - Walmart.com'
+  # end
+
+  it 'Get walmart item for batman search' do
 
     url = 'http://www.walmart.com/search/search-ng.do?search_constraint=0&ic=48_0&search_query=Batman&Find.x=0&Find.y=0&Find=Find'
+
     doc = Nokogiri::HTML(open(url))
-
-    puts "Title: #{doc.at_css('title').text}"
-
-    doc.at_css('title').text.should eq 'Batman - Walmart.com'
+    doc.css('.item').each do |item|
+      text = item.at_css('.prodLink').text
+      price = item.at_css('.PriceXLBold, .PriceCompare .BodyS').text[/\$[0-9\.]+/]
+      puts "#{text} - #{price}"
+    end
   end
 
-  # it 'Get an example image' do
-  #
-  #   PAGE = 'http://www.marketwatch.com'
-  #
-  #   html = Nokogiri.HTML(open(PAGE))
-  #
-  #   src  = html.at('#logo')['background-image']['url']
-  #
-  #   File.open('foo.png', 'wb') do |f|
-  #     f.write(open(src).read)
-  #   end
-  # end
+  it 'Get an marketwatch image' do
+
+    PAGE = 'http://www.marketwatch.com'
+
+    html = Nokogiri.HTML(open(PAGE))
+
+    src  = html.at('#logo')['background-image']['url']
+
+    File.open('foo.png', 'wb') do |f|
+      f.write(open(src).read)
+    end
+  end
 end
