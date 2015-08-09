d8152fddb2cf43320eb8fd9d8f304925ca0bffc8
David Jimenez <david.jimenez19@gmail.com>
2015-08-09 13:14:34 +0200

    44729215118886 Display calendar events
    - define  the time frame option in the controller
    - selected the time frame option base on the the time frame parameter
    - Fix 2 test, 3 still failing

--- app/controllers/trading_controller.rb
+++ app/controllers/trading_controller.rb
@@ -27,9 +27,11 @@ class TradingController < ApplicationController
     @calendar_snapshot = @calendar_service.get_weekly_calendar_data(time_frame, cal_type)
     @from = '22'
     @to = '233'
+    @time_frame_options = build_time_frame_options time_frame
 
     @first_iteration = true
   end
+
 
   def weekly_calendar
 
@@ -52,5 +54,35 @@ class TradingController < ApplicationController
 
   def set_time_parameter
     @time = Time.now.to_s(:datetime_military)
+  end
+
+  def build_time_frame_options(time_frame)
+
+    [{
+         :value => 'thisWeek',
+         :text => 'This week',
+         :selected => time_frame == 'thisWeek'
+     }, {
+         :value => 'nextWeek',
+         :text => 'Next Week',
+         :selected => time_frame == 'nextWeek'
+     }, {
+         :value => 'lastWeek',
+         :text => 'Last Week',
+         :selected => time_frame == 'lastWeek'
+     }, {
+         :value => 'today',
+         :text => 'Today',
+         :selected => time_frame == 'today'
+     }, {
+         :value => 'tomorrow',
+         :text => 'Tomorrow',
+         :selected => time_frame == 'tomorrow'
+     }, {
+         :value => 'yesterday',
+         :text => 'Yesterday',
+         :selected => time_frame == 'yesterday'
+     }
+    ]
   end
 end
