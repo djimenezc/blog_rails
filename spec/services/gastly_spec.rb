022c7be97054601298875cf9ba1e124fdc0cb612
David Jimenez <david.jimenez19@gmail.com>
2016-12-09 18:04:01 +0100

    Gastly is getting the chart from stockcharts

--- spec/services/gastly_spec.rb
+++ spec/services/gastly_spec.rb
@@ -20,7 +20,22 @@ describe ScreenScrapperService do
 
     image.resize(width: 110, height: 110) # Creates a previews of web-page
     image.format('png')
-    image.save(TMP_DIRECTORY + 'googleLogo.jpg')
+    image.save(TMP_DIRECTORY + 'googleLogo.png')
+  end
+
+  it 'Take a page screenshot stockcharts.com-ushl.jpg' do
+
+    screenshot = Gastly.screenshot('http://stockcharts.com/h-sc/ui?s=$USHL5&p=D&yr=8&mn=0&dy=0&id=p27747499092')
+    screenshot.selector = '.chartImg-container' # By default, the full screen is captured
+    screenshot.browser_width = 1280 # Default: 1440px
+    screenshot.browser_height = 780 # Default: 900px
+    screenshot.timeout = 1000 # Default: 0 seconds
+    screenshot.cookies = { user_id: 1, auth_token: 'abcd' } # If you need
+    screenshot.phantomjs_options = '--ignore-ssl-errors=true'
+    image = screenshot.capture
+
+    image.format('png')
+    image.save(TMP_DIRECTORY + 'stockcharts.com-ushl.png')
   end
 
 end
