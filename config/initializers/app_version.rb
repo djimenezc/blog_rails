04f326b1f20de7a18a87a5f59c9b2e9fb97ac224
David Jimenez <david.jimenez19@gmail.com>
2016-12-10 00:40:34 +0100

    - upgrade version - nh nl image displayed

--- config/initializers/app_version.rb
+++ config/initializers/app_version.rb
@@ -2,7 +2,7 @@ class Configuration
   class << self
     attr_accessor :app_version, :git_version
   end
-  @app_version = '1.0.2'
+  @app_version = '1.0.3'
   @git_version = `git show --pretty=%H`[0..39]
 
 end
